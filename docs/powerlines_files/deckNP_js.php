691ee969cae1cb68423aedd9b12075491344cc88
Rene Gomez <rene@sirius.utp.edu.co>
2016-09-18 12:36:49 -0500

    added chec demo

---
+++ docs/powerlines_files/deckNP_js.php
@@ -0,0 +1,17 @@
+// display the advertisement
+
+var advert = ''
+
+advert+='<a href="https://www.contentful.com/?utm_campaign=deck-2016-02&utm_medium=cpc&utm_source=decknetwork.net&utm_content=deck-no-wp&utm_term=">';
+advert+='<img class="ad" alt="Contentful" src="//connect.decknetwork.net/i/contentful_4.png" border="0" width="120" height="90" />';
+advert+='</a>';
+advert+='<p class="ads">';
+advert+='Manage content via an API. <a href="https://www.contentful.com/?utm_campaign=deck-2016-02&utm_medium=cpc&utm_source=decknetwork.net&utm_content=deck-no-wp&utm_term=">Meet Contentful</a>, a content management developer platform.';
+advert+='</p>';
+
+
+
+
+document.write(advert);
+
+
